3d4dfff2e647add0a0c4c77d318d5e214d873312
Ann <example@gmail.com>
2021-07-21 14:32:19 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
   "use script";
 
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// ++incr;
-// --decr;
 
-console.log(++incr);
-console.log(--decr);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
-console.log(5%2);
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его?', '');
 
-console.log(2 + 2 * 2 === 8);
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
 
-const isChecked = false;
-      isClose = false;
-
-      console.log(isChecked || !isClose);
+ console.log(personalMovieDB);
